4aada753c6ee8a25c9183722e4890b6d514a6378
Mahesh <maheshkhatri2058@gmail.com>
2024-04-16 15:39:12 +0530

    new files are added

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+function toggleNavbar() {
+  var navbar = document.getElementById("navbar");
+  if (navbar.className === "navbar") {
+    navbar.className += " responsive";
+  } else {
+    navbar.className = "navbar";
+  }
+}
